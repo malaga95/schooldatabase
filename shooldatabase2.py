860e490eaf744e91e568b741f3308ea13718a6f4
Oskar Okroj <ookroj95@gmail.com>
2021-05-13 00:20:39 +0200

    new conception

---
+++ shooldatabase2.py
@@ -0,0 +1,105 @@
+class Tutor: #Nauczyciel ma przedmioty oraz klasy ktore uczeszczaja na dany przedmiot.
+             #{Tadeusz nowak : {2a:[biologia]} #jak odwolac się do metody wewnatrz klasy poza nia ?
+             # Amadeusz Powrot :[]}
+    def __init__(self, name, subject, group):
+        self.name = name
+        self.subject = subject
+        self.group = group
+    def data(self):
+        #Dictonary that contains data of tutor
+        data = {}
+        new_dict = {}
+        subjects = []
+        subjects.append(subject)
+        new_dict = {self.group : subjects}
+        data = {self.name : new_dict}
+        return data
+
+
+class Student:
+    #Struktura danych 
+        #{Andrzej nowak : 3c
+        # Marcin niebrzydki :3a
+        # }
+    def __init__(self, name, group):
+        self.name = name
+        self.group = group
+   
+    def data(self):
+        data={}
+        data = {self.name : self.group}
+        return data
+    '''def klasa_przypis(self):
+        if group in students_in_class:
+            students.append(name)
+            students_in_class[group] = students
+        else:
+            students = []
+            students.append(name)
+            students_in_class[group] = students
+        return students_in_class'''
+
+class Keeper:
+    #Struktura
+    #{Tadeusz Nowak :[]
+    # Amadeusz Pi :[]}
+    def __init__(self, name, group):
+        self.name = name
+        self.group = group
+    def data(self):
+        data={}
+        klasy=[]
+        data[self.name] = [self.group]
+        return data
+#w jaki sposob zapisywac wprowadzone dane w "kodzie"
+#w jaki sposob wyciagac dane ? np drukowac klasy uczniow
+#tworzyc nowa strukture po wykonaniu intrukcji
+
+
+students_in_class = {}   #{3a:[tadek,andrzej,mateusz]}
+students = []
+keepers = []
+tutors = []
+sample_dict = {}
+list_of_students = []
+probing = {}
+roles = ('Wychowawca', 'Nauczyciel', 'Uczen','wychowawca', 'nauczyciel', 
+    'uczen', 'koniec', 'Koniec')   
+while True:
+    role = input('kim jestes ?')
+    if role == "Nauczyciel" or role == 'nauczyciel':
+        name = input('Podaj swoje imie i nazwisko :\n')
+        subject = input('jakiego przedmiotu nauczasz ?\n')
+        students_group = input('jakiej klasy uczysz tego przedmiotu? \n')
+        personal_data = Tutor(name, subject, students_group)
+        print_data = personal_data.data()
+        if students_group in print_data:
+            subject.append(subject)
+        tutors.append(print_data)
+        print(tutors)
+    if role == "Uczen" or role == 'uczen':
+        name = input('Podaj swoje imie i nazwisko :\n')
+        group = input('Podaj numer swojej klasy\n')
+        personal_data = Student(name, group) #wywoluje klase
+        print_data = personal_data.data() #wykonuje metode na obiekcie
+        '''klasa = personal_data.klasa_przypis()'''
+        if group in students_in_class: #nie dziala mi w metodzie, prawdopodobnie z powodu pustego atrybutu
+            students.append(name)
+            students_in_class[group] = students
+        else:
+            students = []
+            students.append(name)
+            students_in_class[group] = students
+        #students_in_class = {group: students}
+        print(students_in_class)
+        #klasa1 = personal_data.klasa_przypis()
+        
+         #dodaje studentow do listy w klasie
+        #lista studentów potrzebny if
+    if role == "Wychowawca" or role == 'wychowawca':
+        name = input('Podaj swoje imie i nazwisko :\n')
+        group = input('Podaj numer klasy, ktorej jestes opiekunem\n')
+        personal_data = Keeper(name, group)
+        print_data = personal_data.data()
+        print(print_data)
+
